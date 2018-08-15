e26f08aa13695cd6f882832592b7e68dbd4464ef
Zachary Sunberg <sunbergzach@gmail.com>
2018-08-15 14:58:54 -0700

    cut down previous observation updater

--- src/previous_observation.jl
+++ src/previous_observation.jl
@@ -4,33 +4,10 @@
 # policies based on the previous observation only are often pretty good
 # e.g. for the crying baby problem
 """
-Updater that stores the most recent observation as the belief.
-
-The belief is Nullable and is null if there is no observation available.
+Updater that stores the most recent observation as the belief, missing if none is available.
 """
-struct PreviousObservationUpdater{O} <: Updater end
+struct PreviousObservationUpdater <: Updater end
 
-initialize_belief(u::PreviousObservationUpdater{O}, d::Any, b=nothing) where {O} = nothing
-initialize_belief(u::PreviousObservationUpdater{O}, o::O, b=nothing) where {O} = o
+initialize_belief(u::PreviousObservationUpdater, d::Any) = missing
 
-update(bu::PreviousObservationUpdater{O}, old_b, action, obs::O, b=nothing) where {O} = obs
-
-"""
-Updater that stores the most recent observation as the belief.
-"""
-struct FastPreviousObservationUpdater{O} <: Updater end
-
-# the only way this belief can be initialized is with a correct observation
-initialize_belief(u::FastPreviousObservationUpdater{O}, o::O) where O = o
-update(bu::FastPreviousObservationUpdater{O}, old_b, action, obs::O) where O = obs
-
-"""
-Updater that stores the most recent observation as the belief.
-
-On the first step (when initialize_belief is called), it uses the default.
-"""
-struct PrimedPreviousObservationUpdater{O} <: Updater
-    default::O
-end
-initialize_belief(u::PrimedPreviousObservationUpdater, b) = u.default
-update(u::PrimedPreviousObservationUpdater{O}, old_b, action, obs::O) where O = obs
+update(bu::PreviousObservationUpdater, old_b, action, obs) = obs
