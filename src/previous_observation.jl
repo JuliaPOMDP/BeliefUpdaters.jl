6edc5517fc46b3b46d3bfa8fffc9d81fa60fc4ff
Zachary Sunberg <sunbergzach@gmail.com>
2018-08-16 15:51:22 -0700

    PreviousObservationUpdater initialize_belief
    Should this return missing or the distribution?

--- src/previous_observation.jl
+++ src/previous_observation.jl
@@ -8,6 +8,6 @@ Updater that stores the most recent observation as the belief, missing if none i
 """
 struct PreviousObservationUpdater <: Updater end
 
-initialize_belief(u::PreviousObservationUpdater, d::Any) = missing
+initialize_belief(u::PreviousObservationUpdater, d::Any) = d
 
 update(bu::PreviousObservationUpdater, old_b, action, obs) = obs
