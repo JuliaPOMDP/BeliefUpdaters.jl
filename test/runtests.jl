e4facdca0e4e300acf20a63569769455164b5f81
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-13 12:46:16 -0700

    enabled tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using Test
 using POMDPs
 using POMDPModelTools
 using BeliefUpdaters
+using POMDPModels
 using Random
 
-#XXX tests require POMDPModels, uncomment once POMDPModels is merged
-# @testset "belief" begin
-#     include("test_belief.jl")
-# end
+@testset "belief" begin
+    include("test_belief.jl")
+end
 
-# @testset "kprevobs" begin
-#     include("test_k_previous_observations_belief.jl")
-# end
+@testset "kprevobs" begin
+    include("test_k_previous_observations_belief.jl")
+end
