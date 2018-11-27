af081836a9fe1a4dd76f3a8627dc6397b03dbfb0
autodocs <autodocs>
2018-11-27 20:51:30 +0000

    build based on 3f716b9

--- latest/search_index.js
+++ latest/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Discrete",
     "title": "Discrete",
     "category": "section",
-    "text": "BeliefUpdaters contains a default implementation of a discrete bayesian filter. The updater is defined with the DiscreteUpdater type. The DiscreteBelief type is provided to represent discrete beliefs for discrete states POMDPs. A convenience function uniform_belief is probided to create a DiscreteBelief with equal probability for each state. DiscreteBeliefDiscreteUpdateruniform_belief(pomdp)"
+    "text": "BeliefUpdaters contains a default implementation of a discrete bayesian filter. The updater is defined with the DiscreteUpdater type. The DiscreteBelief type is provided to represent discrete beliefs for discrete state POMDPs. A convenience function uniform_belief is provided to create a DiscreteBelief with equal probability for each state. DiscreteBeliefDiscreteUpdateruniform_belief(pomdp)"
 },
 
 {
