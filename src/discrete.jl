470ed9332aa3c6357e91b01dc13446e7fb3c2d0a
Wout Schellaert <wout.schellaert@gmail.com>
2020-05-21 08:57:16 +0200

    Fix typo

--- src/discrete.jl
+++ src/discrete.jl
@@ -8,7 +8,7 @@
 
 A belief specified by a probability vector.
 
-Normalization of `b` is assumed in some calculations (e.g. pdf), but it is only automatically enforced in `update(...)`, and a warning is given if normalized incorrectly in `DiscreteBeleif(pomdp, b)`.
+Normalization of `b` is assumed in some calculations (e.g. pdf), but it is only automatically enforced in `update(...)`, and a warning is given if normalized incorrectly in `DiscreteBelief(pomdp, b)`.
 
 # Constructor
     DiscreteBelief(pomdp, b::Vector{Float64}; check::Bool=true)
